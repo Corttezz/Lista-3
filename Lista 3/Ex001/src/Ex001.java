7adc02703a32bc2151f5a06fe52c4389b5ec61e5
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2022-09-22 03:06:06 -0300

    Add files via upload

---
+++ Lista 3/Ex001/src/Ex001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+// Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
+// e continue pedindo até que o usuário informe um valor válido.
+public class App {
+    public static void main(String[] args) throws Exception {
+      int num;
+      Scanner tc = new Scanner(System.in);
+      System.out.print("Digite um número de 0 a 10: ");
+      num = tc.nextInt();
+      while(num < 0 || num>10){
+        System.out.println("Número inválido. Digite um número de 0 a 10: ");
+        num = tc.nextInt();
+      }
+      tc.close();
+    }
+}
